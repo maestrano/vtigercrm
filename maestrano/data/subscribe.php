b49e0f0fc759443fc060b1248fa061f8c4850e83
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-01-28 17:10:05 +1100

    remove semphore lock - not working on UAT

--- maestrano/data/subscribe.php
+++ maestrano/data/subscribe.php
@@ -8,69 +8,59 @@ if (!defined('MAESTRANO_ROOT')) {
 }
 require_once(MAESTRANO_ROOT . '/app/init/soa.php');
 
-// Semaphore to prevent multi-threading
-if (!defined('SEM_KEY')) {
-  define('SEM_KEY', 1000);
-}
-$semRes = sem_get(SEM_KEY, 1, 0666, 1);
-
 $maestrano = MaestranoService::getInstance();
 
 if ($maestrano->isSoaEnabled() and $maestrano->getSoaUrl()) {
   // Do not allow concurrent executions of notifications
   try {
-    if(sem_acquire($semRes)) {
-      $log = new MnoSoaBaseLogger();
+    $log = new MnoSoaBaseLogger();
 
-      $notification = json_decode(file_get_contents('php://input'), false);
-      $notification_entity = strtoupper(trim($notification->entity));
-      
-      $log->debug("Notification = ". json_encode($notification));
-      
-      switch ($notification_entity) {
-  	    case "COMPANY":
-                if (class_exists('MnoSoaCompany')) {
-                  $mno_company = new MnoSoaCompany($opts['db_connection'], $log);
-                  $mno_company->receiveNotification($notification);
-                }
-        break;
-        case "TAXCODES":
-                if (class_exists('MnoSoaTax')) {
-                  $mno_tax = new MnoSoaTax($opts['db_connection'], $log);
-                  $mno_tax->receiveNotification($notification);
-                }
-        break;
-        case "ORGANIZATIONS":
-                if (class_exists('MnoSoaOrganization')) {
-                  $mno_org = new MnoSoaOrganization($opts['db_connection'], $log);		
-                  $mno_org->receiveNotification($notification);
-                }
-  			break;
-        case "PERSONS":
-                if (class_exists('MnoSoaPerson')) {
-                  $mno_person = new MnoSoaPerson($opts['db_connection'], $log);   
-                  $mno_person->receiveNotification($notification);
-                }
-        break;
-        case "ITEMS":
-                if (class_exists('MnoSoaItem')) {
-                  $mno_item = new MnoSoaItem($opts['db_connection'], $log);   
-                  $mno_item->receiveNotification($notification);
-                }
-        break;
-        case "INVOICES":
-                if (class_exists('MnoSoaInvoice')) {
-                  $mno_invoice = new MnoSoaInvoice($opts['db_connection'], $log);   
-                  $mno_invoice->receiveNotification($notification);
-                }
-        break;
-      }
+    $notification = json_decode(file_get_contents('php://input'), false);
+    $notification_entity = strtoupper(trim($notification->entity));
+    
+    $log->debug("Notification = ". json_encode($notification));
+    
+    switch ($notification_entity) {
+	    case "COMPANY":
+              if (class_exists('MnoSoaCompany')) {
+                $mno_company = new MnoSoaCompany($opts['db_connection'], $log);
+                $mno_company->receiveNotification($notification);
+              }
+      break;
+      case "TAXCODES":
+              if (class_exists('MnoSoaTax')) {
+                $mno_tax = new MnoSoaTax($opts['db_connection'], $log);
+                $mno_tax->receiveNotification($notification);
+              }
+      break;
+      case "ORGANIZATIONS":
+              if (class_exists('MnoSoaOrganization')) {
+                $mno_org = new MnoSoaOrganization($opts['db_connection'], $log);		
+                $mno_org->receiveNotification($notification);
+              }
+			break;
+      case "PERSONS":
+              if (class_exists('MnoSoaPerson')) {
+                $mno_person = new MnoSoaPerson($opts['db_connection'], $log);   
+                $mno_person->receiveNotification($notification);
+              }
+      break;
+      case "ITEMS":
+              if (class_exists('MnoSoaItem')) {
+                $mno_item = new MnoSoaItem($opts['db_connection'], $log);   
+                $mno_item->receiveNotification($notification);
+              }
+      break;
+      case "INVOICES":
+              if (class_exists('MnoSoaInvoice')) {
+                $mno_invoice = new MnoSoaInvoice($opts['db_connection'], $log);   
+                $mno_invoice->receiveNotification($notification);
+              }
+      break;
     }
   } catch (Exception $e) {
     $log->debug("Caught exception in subscribe " . json_encode($e->getMessage()));
   }
-  
-  sem_release($semRes);
 }
 
 ?>
