c22679ba90ba17f5f9818e85b20a9cdd61f38337
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-02-01 14:37:35 +1100

    Install Event and Ticket modules and configure related_to fields

---
+++ modules/Ticketss/language/en_us.lang.php
@@ -0,0 +1,32 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$mod_strings = Array(
+	'Ticketss' => 'Tickets',
+	'SINGLE_Ticketss' => 'Tickets',
+	'ModuleName ID' => 'Tickets ID',
+
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_MODULEBLOCK_INFORMATION' => 'ModuleBlock Information',
+
+	'ModuleFieldLabel' => 'ModuleFieldLabel Text',
+
+'Contacts' => 'Contacts',
+'Event' => 'Event Management',
+	
+
+);
+
+
+
+global $app_strings;
+$app_strings['Ticketss'] = 'Tickets';
+	
+?>
