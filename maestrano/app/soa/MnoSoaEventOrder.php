a3b496fd09f41a0df559396e31caf17c1bf1488b
BrunoChauvet <bruno.chauvet@maestrano.com>
2015-02-23 16:49:20 +1100

    Map Organization membership type to Ticket type

--- maestrano/app/soa/MnoSoaEventOrder.php
+++ maestrano/app/soa/MnoSoaEventOrder.php
@@ -4,6 +4,10 @@ if (!defined('APP_DIR')) {
   define("APP_DIR", realpath(dirname(__FILE__) . '/../../../'));
 }
 chdir(APP_DIR);
+
+require_once 'vtlib/Vtiger/Field.php';
+require_once 'vtlib/Vtiger/Module.php';
+
 if(file_exists ('modules/Event/Event.php')) {
   require_once 'modules/Event/Event.php';
   require_once 'modules/Tickets/Tickets.php';
@@ -34,22 +38,25 @@ class MnoSoaEventOrder extends MnoSoaBaseEventOrder {
       $local_id = $this->getLocalIdByMnoId($this->_id);
       $this->_log->debug(__FUNCTION__ . " this->getLocalIdByMnoId(this->_id) = " . json_encode($local_id));
       if ($this->isValidIdentifier($local_id)) {
-        $status = constant('MnoSoaBaseEntity::STATUS_EXISTING_ID');
+        $this->_log->debug("Event Order already processed, skip");
+        return constant('MnoSoaBaseEntity::STATUS_EXISTING_ID');
       } else if ($this->isDeletedIdentifier($local_id)) {
         $this->_log->debug(__FUNCTION__ . " is STATUS_DELETED_ID");
-        $status = constant('MnoSoaBaseEntity::STATUS_DELETED_ID');
+        $this->_log->debug("Event Order deleted, skip");
+        return constant('MnoSoaBaseEntity::STATUS_DELETED_ID');
       } else {
         $status = constant('MnoSoaBaseEntity::STATUS_NEW_ID');
       }
     } else {
-      $status = constant('MnoSoaBaseEntity::STATUS_ERROR');
+      return constant('MnoSoaBaseEntity::STATUS_ERROR');
     }
 
     // Map the Event
     $this->_log->debug("map EventOrder related Event " . $this->_event_id);
-    if($this->_event_id) {
-      $event_id = $this->getLocalIdByMnoIdName($this->_event_id, "EVENTS");
-    } else {
+    if(is_null($this->_event_id)) { return null; }
+   
+    $event_id = $this->getLocalIdByMnoIdName($this->_event_id, "EVENTS");
+    if(is_null($event_id)) {
       $this->_log->debug("Event Id missing, fetching entity_id=" . $this->_event_id);
       
       $notification->entity = "events";
@@ -61,6 +68,9 @@ class MnoSoaEventOrder extends MnoSoaBaseEventOrder {
         if($event_id == null) { return constant('MnoSoaBaseEntity::STATUS_ERROR'); }
       }
     }
+    // Fetch the Event
+    $vtiger_event = CRMEntity::getInstance("Event");
+    $vtiger_event->retrieve_entity_info($event_id->_id, "Event");
 
     // Map the attendees
     foreach ($this->_attendees as $attendee) {
@@ -78,18 +88,49 @@ class MnoSoaEventOrder extends MnoSoaBaseEventOrder {
             $person_id = $this->getLocalIdByMnoIdName($attendee->person->id, "PERSONS");
           }
         }
+        if(is_null($person_id)) { return null; }
 
-        if($person_id) {
-          // Fetch the Contact
-          $vtiger_contact = CRMEntity::getInstance("Contacts");
-          $vtiger_contact->retrieve_entity_info($person_id->_id, "Contacts");
-          $vtiger_contact->id = $person_id->_id;
-          $account_id = $vtiger_contact->column_fields['account_id'];
-          // Fetch the Contact Organization
-          $vtiger_account = CRMEntity::getInstance("Accounts");
-          $vtiger_account->retrieve_entity_info($account_id, "Accounts");
-          // Register the Contact to the Event
-          $vtiger_account->save_related_module('Event', $event_id->_id, 'Contact', $person_id->_id);
+        // Fetch the Contact
+        $vtiger_contact = CRMEntity::getInstance("Contacts");
+        $vtiger_contact->retrieve_entity_info($person_id->_id, "Contacts");
+        $vtiger_contact->id = $person_id->_id;
+        $account_id = $vtiger_contact->column_fields['account_id'];
+        // Fetch the Contact Organization
+        $vtiger_account = CRMEntity::getInstance("Accounts");
+        $vtiger_account->retrieve_entity_info($account_id, "Accounts");
+        // Register the Contact to the Event
+        $vtiger_account->save_related_module('Event', $event_id->_id, 'Contact', $person_id->_id);
+
+        // Map Organization Membership Type onto Ticket
+        $organization_membership_label = $this->findFieldByLabel('Accounts', 'Membership Type');
+        if($organization_membership_label) {
+          // Find Ticket Membership Type label
+          $ticket_membership_label = $this->findFieldByLabel('Tickets', 'Membership Type');
+          if($ticket_membership_label) {
+            // Fetch Organization membership type
+            $organization_membership = $vtiger_account->column_fields[$organization_membership_label->name];
+            if(is_null($organization_membership)) { $organization_membership = ''; }
+
+            // Find or Create Ticket of this Membership Type
+            $ticket = $this->findTicketByEventAndType($event_id->_id, $organization_membership);
+
+            if(is_null($ticket)) {
+              $ticket = new Tickets();
+              $ticket->column_fields['assigned_user_id'] = "1";
+              $ticket->column_fields['tksevent'] = $event_id->_id;
+              $ticket->column_fields['tksticketname'] = $vtiger_event->column_fields['tkseventname'] . " - " . $organization_membership;
+              $ticket->column_fields['tksdescription'] = $vtiger_event->column_fields['tkseventname'] . " - " . $organization_membership;
+              $ticket->column_fields['tickets_tksmembershiptype'] = $organization_membership;
+              $ticket->column_fields['tksamountavailable'] = null;
+              $ticket->column_fields['tksticketprice'] = null;
+              $ticket->column_fields['tksstartdate'] = $vtiger_event->column_fields['tksdateofevent'];
+              $ticket->column_fields['tksenddate'] = null;
+              $ticket->save('Tickets');
+            }
+
+            // Link the Contacts to the Ticket
+            $vtiger_event->save_related_module('Tickets', $ticket->id, 'Contact', $vtiger_contact->id);
+          }
         }
       }
     }
@@ -103,7 +144,33 @@ class MnoSoaEventOrder extends MnoSoaBaseEventOrder {
   }
 
   public function getLocalEntityIdentifier() {
-    return $this->_local_entity->id;
+    return 1;
+  }
+
+  private function findFieldByLabel($module_name, $field_label) {
+    $account_module = Vtiger_Module::getInstance($module_name);
+    $fields = Vtiger_Field::getAllForModule($account_module);
+    foreach ($fields as $field) {
+      if($field->label == $field_label) {
+        return $field;
+      }
+    }
+    return null;
+  }
+
+  private function findTicketByEventAndType($event_id, $membership_type) {
+    $ticket = CRMEntity::getInstance("Tickets");
+    $query = $ticket->getListQuery('Tickets', ' AND vtiger_tickets.tksevent = ' . $event_id . " AND vtiger_tickets.tickets_tksmembershiptype = '" . $membership_type . "'");
+    $result = $this->_db->pquery($query, array());
+    if($result) {
+      foreach ($result as $event_ticket) {     
+        $ticket_id = $event_ticket['crmid'];
+        $ticket->retrieve_entity_info($ticket_id, "Tickets");
+        $ticket->id = $ticket_id;
+        return $ticket;
+      }
+    }
+    return null;
   }
 }
 
